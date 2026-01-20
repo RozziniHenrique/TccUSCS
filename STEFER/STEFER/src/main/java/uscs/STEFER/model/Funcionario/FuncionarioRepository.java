4cb5e4d8847d9c6ebcad337b66ef0d6f5e077c54
Henrique <hrozzini@gmail.com>
2026-01-20 10:09:55 -0300

    Delete STEFER/STEFER directory

--- STEFER/STEFER/src/main/java/uscs/STEFER/model/Funcionario/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package uscs.STEFER.model.Funcionario;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-    Page<Funcionario> findAllByAtivoTrue(Pageable paginacao);
-}
